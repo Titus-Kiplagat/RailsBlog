033e4595b2df14ba15190d2eca308c43456751c2
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-12-20 14:09:29 +0300

    Add foreign key column

--- db/migrate/20231214184400_create_posts.rb
+++ db/migrate/20231214184400_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.references :author, foreign_key: { to_table: :users, index: true }
+      t.references :author, foreign_key: { to_table: :users, column: :author_id, index: true }
       t.string :title
       t.text :text
       t.integer :likes_counter, default: 0
