f6678f983b80a08299662be250beb96a864118d4
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-02-08 13:10:46 +0300

    Add instance method method that updates posts counter for a user method that returns 5 recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def update_posts_counter
+    Post.group(:author_id).count.each do |author_id, posts_count|
+      User.find(author_id).update(posts_counter: posts_count)
+    end
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
