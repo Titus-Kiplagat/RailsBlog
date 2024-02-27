0c6aa64a9fa242cb006763baca45dfa5d48d9ea9
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-02-27 15:09:42 +0300

    Refactor PostsController to load current user before index and show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
+  before_action :current_user, only: %i[index show]
+
   def index
+    @posts = @user.posts.paginate(page: params[:page], per_page: 2).order('created_at DESC')
   end
 
   def show
+    @post = @user.posts.find(params[:id])
+  end
+
+  private
+
+  def current_user
+    @user = User.find(params[:user_id])
   end
 end
